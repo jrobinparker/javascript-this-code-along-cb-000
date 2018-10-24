3a0bc160ccdf52b9fa059a998172b1ea0b18322c
Jeremy Parker <jrobinparker@gmail.com>
2018-10-24 18:40:21 +0000

    Completed lesson

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  };
+  this.serve = function() {
+    console.log("here's your " + this.name + ", enjoy!")
+  };
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table #" + this.tableNumber)
+}
+
+let pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PBJ");
+
+let salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+
+salad.describe = pbj.describe.bind(salad);
+
+let sally = new Customer("Sally", "4");
+
+let visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
